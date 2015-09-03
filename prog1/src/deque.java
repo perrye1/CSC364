de1d48c26e6db7ac511d7a35179b87ac2694ae2b
perrye1 <perrye1@nku.edu>
2015-09-03 14:58:43 -0400

    added some methods

--- prog1/src/deque.java
+++ prog1/src/deque.java
@@ -1,3 +1,41 @@
 public class deque{
-	//test2
+	private dequeNode elts;
+	
+	public deque(){
+		elts=null;
+	}
+	
+	public void insertFront(int item){
+		dequeNode temp = new dequeNode(item);
+		if(elts==null){
+			elts=temp;
+		}else{
+			temp.setNext(elts);
+			elts=temp;
+		}
+	}
+	
+	public void insertRear(int item){
+		dequeNode temp = new dequeNode(item);
+		if(elts==null){
+			elts=temp;
+		}
+	}
+	
+	public int deleteFront(){
+		
+	}
+	
+	public int deleteRear(){
+		
+	}
+	
+	public boolean isEmpty(){
+		
+	}
+	
+	public void printDeque(){
+		
+	}
+	
 }
