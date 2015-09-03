31a20a0b048dddab2da81226110fb4caa9c20012
perrye1 <perrye1@nku.edu>
2015-09-03 13:56:36 -0400

    created data members and methods for node class

--- prog1/src/dequeNode.java
+++ prog1/src/dequeNode.java
@@ -1,3 +1,26 @@
 public class dequeNode{
 	
+	protected dequeNode next;
+	protected dequeNode prev;
+	protected int val;
+	
+	public dequeNode getNext(){
+		
+	}
+	
+	public dequeNode getPrev(){
+		
+	}
+	
+	public void setNext(){
+		
+	}
+	
+	public void setPrev(){
+		
+	}
+	
+	public int getVal(){
+		
+	}
 }
